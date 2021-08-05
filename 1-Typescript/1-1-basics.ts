7dba36977532851de62a743a0be0032288bc5960
syoungee <syoung_ee@naver.com>
2021-08-05 16:15:49 +0900

    first  commit

---
+++ 1-Typescript/1-1-basics.ts
@@ -0,0 +1,26 @@
+{
+  // JavaScript
+  // old: var
+  // let es6
+  let name = "hello";
+  name = "hi"; // 재할당 가능
+
+  // const
+  const age = 5;
+  age = 5; // error!
+
+  /**
+   * JavaScript
+   * Primitive: number, string, boolean, bigint, symbol, null, undefined
+   * Object: function, array......
+   */
+
+  // number
+  const num: number = -6;
+
+  // string
+  const str: string = "hello";
+
+  //boolean
+  const boal: boolean = true; // true, false
+}
