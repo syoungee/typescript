423d9d5202f75b50f9cc2ac5d8e2fb7af3c8fbf7
syoungee <syoung_ee@naver.com>
2021-08-06 10:34:00 +0900

    2.5 기본 타입 정리3

--- 1-Typescript/1-1-basics.ts
+++ 1-Typescript/1-1-basics.ts
@@ -33,4 +33,35 @@
   // null
   let person: null; // 💩
   let person2: string | null;
+
+  // unknown 💩
+  let notSure: unknown = 0;
+  notSure = "he";
+  notSure = true;
+
+  // any 💩
+  let anything: any = 0;
+  anything = "hello";
+
+  // void
+  function print(): void {
+    console.log("hello");
+    return;
+  }
+
+  let unusable: void = undefined; // 💩
+
+  // never
+  function throwError(message: string): never {
+    // message -> server (log)
+    // throw new Error(message);
+    while (true) {}
+  }
+  let neverEnding: never; //💩
+
+  // object
+  let obj: object; //💩
+  function acceptSomeObject(obj: object) {}
+  acceptSomeObject({ name: "ellie" });
+  acceptSomeObject({ animal: "dog" });
 }
