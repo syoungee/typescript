aa11d0a2ab9ce23cb4ded4f45d1071376a8d8ee5
syoungee <syoung_ee@naver.com>
2021-08-09 11:08:06 +0900

    2.9 타입스크립트의 꽃 type alias

---
+++ 1-Typescript/1-4-alias.ts
@@ -0,0 +1,29 @@
+{
+  /**
+   * Type Aliases
+   */
+  type Text = string;
+  const name: string = "ellie";
+  const address: Text = "korea";
+  type Num = number;
+  type Student = {
+    name: string;
+    age: number;
+  };
+  const student: Student = {
+    name: "ellie",
+    age: 12,
+  };
+
+  /**
+   * String Literal Types
+   */
+  type Name = "name";
+  let ellieName: Name;
+  ellieName = "name";
+  type JSON = "json";
+  const json: JSON = "json";
+
+  type Boal = true;
+  const isCat: Boal = true;
+}
