dede7d96186fe94950ef3a4cc07b30033269ee57
syoungee <syoung_ee@naver.com>
2021-08-09 11:17:58 +0900

    2-10 진정한 타입스크립트의 시작! Union 타입

---
+++ 1-Typescript/1-5-union.ts
@@ -0,0 +1,36 @@
+{
+  /**
+   * Union Types: OR
+   */
+  type Direction = "left" | "right" | "up" | "down";
+  function move(direction: Direction) {
+    console.log(direction);
+  }
+  move("down");
+
+  type TileSize = 8 | 16 | 32;
+  const tile: TileSize = 16;
+
+  // function: login -> success, fail
+  type SuccessState = {
+    response: {
+      body: string;
+    };
+  };
+  type FailState = {
+    reason: string;
+  };
+
+  type LoginState = SuccessState | FailState;
+  function login(): LoginState {
+    return {
+      response: {
+        body: "logged in!",
+      },
+    };
+  }
+
+  // printLoginState(state: LoginState)
+  // success -> 🎉 body
+  // fail -> 🥺 reason
+}
