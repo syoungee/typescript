4654a00677f7c9bb8238c2fcc5ad25899418bfed
syoungee <syoung_ee@naver.com>
2021-08-10 10:20:39 +0900

    2.11 필수 타입 Discriminated Union

--- 1-Typescript/1-6-discriminated.ts
+++ 1-Typescript/1-6-discriminated.ts
@@ -1,29 +1,20 @@
 {
-  /**
-   * Union Types: OR
-   */
-  type Direction = "left" | "right" | "up" | "down";
-  function move(direction: Direction) {
-    console.log(direction);
-  }
-  move("down");
-
-  type TileSize = 8 | 16 | 32;
-  const tile: TileSize = 16;
-
   // function: login -> success, fail
   type SuccessState = {
+    result: "success";
     response: {
       body: string;
     };
   };
   type FailState = {
+    result: "fail";
     reason: string;
   };
 
   type LoginState = SuccessState | FailState;
   function login(): LoginState {
     return {
+      result: "success",
       response: {
         body: "logged in!",
       },
@@ -35,7 +26,8 @@
   // fail -> 🥺 reason
 
   function printLoginState(state: LoginState) {
-    if ("response" in state) {
+    state.result; // success or fail
+    if (state.result === "success") {
       console.log(`🎉${state.response.body}`);
     } else {
       console.log(`😭${state.reason}`);
