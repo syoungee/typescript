1b9f028d538a1000ae3886359e8a74957e387989
syoungee <syoung_ee@naver.com>
2021-08-11 11:08:02 +0900

    3.5 로딩 상태 표시

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,6 +1,28 @@
 /**
  * Let's make a game 🕹
  */
+
+const position = { x: 0, y: 0 };
+type Command = "up" | "down" | "left" | "right";
+function move(direction: Command) {
+  switch (direction) {
+    case "up":
+      position.y++;
+      break;
+    case "down":
+      position.y--;
+      break;
+    case "left":
+      position.x--;
+      break;
+    case "right":
+      position.x++;
+      break;
+    default:
+      throw new Error(`unknown direction ${direction}`);
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
 move("up");
 console.log(position); // { x: 0, y: 1}
