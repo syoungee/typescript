eadd9836489cbaa88a68dcdfd1ccb4db5db8affb
syoungee <syoung_ee@naver.com>
2021-08-11 13:28:21 +0900

    3.7 로딩 상태 표시

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,22 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("👀 loading...");
+        break;
+      case "success":
+        console.log(`😃 ${state.response.body}`);
+        break;
+      case "fail":
+        console.log(`😱 ${state.reason}`);
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
+
   printLoginState({ state: "loading" }); // 👀 loading...
   printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
   printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
