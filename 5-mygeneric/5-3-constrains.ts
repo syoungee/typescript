d84e18285be59b37eeb0cdfc04fd07a43cd8db51
syoungee <syoung_ee@naver.com>
2021-08-18 15:33:10 +0900

    6.5 제네릭 조건 예제

--- 5-mygeneric/5-3-constrains.ts
+++ 5-mygeneric/5-3-constrains.ts
@@ -35,3 +35,20 @@ bob.workPartTime();
 
 const ellieAfterPay = payBad(ellie);
 const bobAfterPay = payBad(bob);
+
+const obj = {
+  name: "ellie",
+  age: 20,
+};
+
+const obj2 = {
+  animal: "🐹",
+};
+
+function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+console.log(getValue(obj, "name")); // ellie
+console.log(getValue(obj, "age")); // 20
+console.log(getValue(obj2, "animal")); // 🐹
