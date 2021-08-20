9f870c25092a747cc3d36a97ef56eedc5d214e43
syoungee <syoung_ee@naver.com>
2021-08-20 11:29:31 +0900

    10.7 Conditional Type

---
+++ 9-advanced/9-4-condition.ts
@@ -0,0 +1,21 @@
+type Check<T> = T extends string ? boolean : number;
+type Type = Check<string>; // boolean
+
+type TypeName<T> = T extends string
+  ? "string"
+  : T extends number
+  ? "number"
+  : T extends boolean
+  ? "boolean"
+  : T extends undefined
+  ? "undefined"
+  : T extends Function
+  ? "function"
+  : "object";
+
+type T0 = TypeName<string>;
+("string");
+type T1 = TypeName<"a">;
+("string");
+type T2 = TypeName<() => void>;
+("function");
