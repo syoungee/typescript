21b9d6c5eb9ce5639aee30d5fd813a7ef0ad57e1
syoungee <syoung_ee@naver.com>
2021-08-20 12:45:44 +0900

    10.9 partial type

---
+++ 9-advanced/9-6-partial.ts
@@ -0,0 +1,19 @@
+{
+  type ToDo = {
+    title: string;
+    description: string;
+    label: string;
+    priority: "high" | "low";
+  };
+  function updateTodo(todo: ToDo, fieldsToUpdate: Partial<ToDo>): ToDo {
+    return { ...todo, ...fieldsToUpdate };
+  }
+  const todo: ToDo = {
+    title: "learn TypeScript",
+    description: "study hard",
+    label: "study",
+    priority: "high",
+  };
+  const updated = updateTodo(todo, { priority: "low" });
+  console.log(updated);
+}
